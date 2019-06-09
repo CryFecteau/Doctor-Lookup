970fb0e0c8bca540ca4ec8ae206ff53e3fe5ff9c
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-08 18:42:16 -0700

    add find doctor by specialty method

--- src/Doctors.js
+++ src/Doctors.js
@@ -17,7 +17,7 @@ export default class Doctors{
     getDoctorsByName(name){
         return new Promise(function(resolve, reject){
             let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=3af7b82cbec1566d6ae4b3986aebd299`;
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=${process.env.apiKey}`;
             request.onload = function(){
                 if(this.status === 200){
                     resolve(request.response);
@@ -26,6 +26,22 @@ export default class Doctors{
                 }
             }
             request.open("GET", url, true);
+        });
+    }
+    // this will find doctor by specialty
+    getDoctorBySpecialty(specialty){
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=${process.env.apiKey}`;
+            request.onload = function(){
+                if(this.status === 200){
+                    resolve(request.statusText);
+                }else{
+                    reject(Error(request.statusText));
+                }
+            }
+            request.open("GET", url, true);
+            request.send();
         });
     }
 }
