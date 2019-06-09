e856be08c554f2d42f85c54918922b3d0ce8e977
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-08 21:01:57 -0700

    refactor and restart

--- src/Doctors.js
+++ src/Doctors.js
@@ -1,47 +1,21 @@
-export default class Doctors{
-    //this will get all the doctors in seattle area
-    getAllDoctors(){
-        return new Promise(function(resolve, reject){
-            let request = new XMLHttpRequest();
-            let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=100&user_key=3af7b82cbec1566d6ae4b3986aebd299';
-            request.onload = function(){
-                if(this.status === 200){
-                    resolve(request.response);
-                }
-            }
-            request.open("GET", url, true);
-            request.send();
-        });
+export class Doctors {
+
+    getDoctors(search){
+  
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${search}&location=wa-seattle&limit=10&user_key=${process.env.exports.apiKey}`;
+  
+        request.onload = function() {
+          if(this.status == 200) {
+            resolve(JSON.parse(request.response));
+          }
+          else {
+            reject(alert(Error(request.statusText)));
+          }
+        };
+        request.open("GET", url, true);
+        request.send();
+      });
     }
-    // this will find a doctor by thier name
-    getDoctorsByName(name){
-        return new Promise(function(resolve, reject){
-            let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=${process.env.apiKey}`;
-            request.onload = function(){
-                if(this.status === 200){
-                    resolve(request.response);
-                }else{
-                    reject(Error(request.statusText));
-                }
-            }
-            request.open("GET", url, true);
-        });
-    }
-    // this will find doctor by specialty
-    getDoctorBySpecialty(specialty){
-        return new Promise(function(resolve, reject){
-            let request = new XMLHttpRequest();
-            let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=${specialty}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=${process.env.apiKey}`;
-            request.onload = function(){
-                if(this.status === 200){
-                    resolve(request.statusText);
-                }else{
-                    reject(Error(request.statusText));
-                }
-            }
-            request.open("GET", url, true);
-            request.send();
-        });
-    }
-}
+  }
