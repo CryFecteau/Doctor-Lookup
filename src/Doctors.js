bd8eafdbafbafdaafa762e5bebedae2f8b0bb03a
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-08 18:29:51 -0700

    add a find doctor by name method

--- src/Doctors.js
+++ src/Doctors.js
@@ -1,5 +1,5 @@
 export default class Doctors{
-    //get all the doctors in seattle area
+    //this will get all the doctors in seattle area
     getAllDoctors(){
         return new Promise(function(resolve, reject){
             let request = new XMLHttpRequest();
@@ -13,5 +13,19 @@ export default class Doctors{
             request.send();
         });
     }
-
+    // this will find a doctor by thier name
+    getDoctorsByName(name){
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=40&user_key=3af7b82cbec1566d6ae4b3986aebd299`;
+            request.onload = function(){
+                if(this.status === 200){
+                    resolve(request.response);
+                }else{
+                    reject(Error(request.statusText));
+                }
+            }
+            request.open("GET", url, true);
+        });
+    }
 }
