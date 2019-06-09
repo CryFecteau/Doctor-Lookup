88499fdb53d6121a9cad3b1099c1e981a1f807c5
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-08 18:19:58 -0700

    add doctors js with a get all method

---
+++ src/Doctors.js
@@ -0,0 +1,17 @@
+export default class Doctors{
+    //get all the doctors in seattle area
+    getAllDoctors(){
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=47.608013%2C%20-122.335167%2C100&user_location=47.608013%2C%20-122.335167&skip=0&limit=100&user_key=3af7b82cbec1566d6ae4b3986aebd299';
+            request.onload = function(){
+                if(this.status === 200){
+                    resolve(request.response);
+                }
+            }
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
+
+}
