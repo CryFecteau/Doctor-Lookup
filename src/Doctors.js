b955fe44523940307aa8c6c250a749267bc8a2a2
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-08 21:07:29 -0700

    add table to input data

--- src/Doctors.js
+++ src/Doctors.js
@@ -4,7 +4,7 @@ export class Doctors {
   
       return new Promise(function(resolve, reject){
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${search}&location=wa-seattle&limit=10&user_key=${process.env.exports.apiKey}`;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${search}&location=wa-seattle&limit=25&user_key=${process.env.exports.apiKey}`;
   
         request.onload = function() {
           if(this.status == 200) {
