f2651428f7c5bef3b86aa0341d60532f2672a437
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-06-09 17:52:48 -0700

    add url to get doctors by issue and name

---
+++ src/doctor.js
@@ -0,0 +1,32 @@
+export class Doctor{
+  getByIssue(issue){
+    return new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5122%2C%20-122.6587%2C%2030&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        }else{
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  getByName(name){
+    return new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5122%2C%20-122.6587%2C%2030&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
+        if(this.status == 200){
+          resolve(request.response);
+        }else{
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
